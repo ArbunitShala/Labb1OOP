ba803b99691811924d13de6ebf4d58bd51e2050e
ArbunitShala <arbunitshalaa@gmail.com>
2024-10-27 19:21:47 +0100

    Add project files.

---
+++ Labb1OOP/Circle.cs
@@ -0,0 +1,17 @@
+﻿namespace Labb1OOP
+{
+    public class Circle
+    {
+        float _pi = 3.141f;
+        int Radius;
+        public Circle(int radius)
+        {
+            Radius = radius;//initierar radien 
+        }
+
+        public float GetArea()
+        {
+            return Radius * Radius * _pi;//metod för att beräkna arean
+        }
+    }
+}
---
+++ Labb1OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Labb1OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);//nytt objekt av klassen Circle
+            Circle circle2 = new Circle(6);
+
+            float area1 = circle1.GetArea();// räkna ut arean för cirklarna
+            float area2 = circle2.GetArea();
+
+            Console.WriteLine($"Arean för cirkeln med radien 5 blir: {area1}");
+            Console.WriteLine($"Arean för cirkeln med radien 6 blir: {area2}");
+        }
+    }
+}
